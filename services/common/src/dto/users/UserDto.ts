6b1186e74c833d9a0ac7e5b0a4187d20138396a9
ValenkoDV <valenkodv@energomera.ru>
2025-09-24 17:00:13 +0300

    Исправлен конфиг. Доработаны контроллеры для health-check

--- services/common/src/dto/users/UserDto.ts
+++ services/common/src/dto/users/UserDto.ts
@@ -1,4 +1,4 @@
-export class UserDto {
+export type UserDto = {
   id: string;
   email: string;
   firstName: string;
--- services/common/src/index.ts
+++ services/common/src/index.ts
@@ -1,6 +1,7 @@
 export * from './constants';
 export * from './enums';
 export * from './dto';
+export * from './interfaces';
 
 export { EnvConfigService } from './config/config.service';
 
