da15236fe199e4048f1c47a6930fafa5748bc490
dimavalencko <dima.valencko@yandex.ru>
2025-10-29 20:14:25 +0300

    Добавлены новые сервисы, удалены ненужные

---
+++ services/identity/src/infrastructure/persistence/typeorm/transaction/transaction-manager.ts
@@ -0,0 +1,12 @@
+import { DataSource, EntityManager } from 'typeorm';
+
+// Позволяет выполнять произвольный блок кода в рамках одной БД-транзакции.
+export class TransactionManager {
+  constructor(private readonly dataSource: DataSource) {}
+
+  async execute<T>(work: (ctx: { manager: EntityManager }) => Promise<T>): Promise<T> {
+    return this.dataSource.transaction(async (manager) => {
+      return work({ manager });
+    });
+  }
+}
