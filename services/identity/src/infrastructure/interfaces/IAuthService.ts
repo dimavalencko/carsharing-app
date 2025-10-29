72af79f47c62201e9adaefef5433166bfda49493
dimavalencko <dima.valencko@yandex.ru>
2025-10-29 17:45:25 +0300

    [fix] - Удален неиспользуемый код

--- services/identity/src/infrastructure/interfaces/IAuthService.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@domain/entities/user.entity';
-import { TokensDto } from '@carsharing/common';
-
-export interface IAuthService {
-  validateUser(email: string, password: string): Promise<User>;
-  login(user: User): Promise<TokensDto>;
-  refreshToken(refreshToken: string): Promise<TokensDto>;
-  logout(userId: string): Promise<void>;
-}
