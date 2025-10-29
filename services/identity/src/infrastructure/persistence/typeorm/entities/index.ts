beb8a981a705caa651b266bdcd975cee8c664e87
dimavalencko <dima.valencko@yandex.ru>
2025-10-29 17:46:26 +0300

    [fix] удалены сущности

--- services/identity/src/infrastructure/persistence/typeorm/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user.entity';
-export * from './user-role.entity';
-export * from './driver-license.entity';
-export * from './user-profile.entity';
-export * from './refresh-token.entity';
