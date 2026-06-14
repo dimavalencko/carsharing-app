d254a292c163aacce85d79080dbdf4a2cf8a3eca
dimavalencko <dima.valencko@yandex.ru>
2026-06-14 14:35:07 +0300

    fix: change-version

--- client/src/pages/admin/admin-layout.vue
+++ client/src/pages/admin/admin-layout.vue
@@ -75,7 +75,7 @@ async function handleLogout() {
       </nav>
 
       <div class="sidebar__footer">
-        <div class="sidebar__version">v1.0.0</div>
+        <div class="sidebar__version">v1.0.1</div>
         <button class="sidebar__logout" @click="handleLogout">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
