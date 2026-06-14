55d77877c8eb6d0689497f9fea1ed3d0e9c468b5
dimavalencko <dima.valencko@yandex.ru>
2026-06-14 13:59:58 +0300

    fix: proxy

--- client/src/app/axios/defaultHttpClient.ts
+++ client/src/app/axios/defaultHttpClient.ts
@@ -1,9 +1,6 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
 import { setupInterceptors } from './interceptors';
 
-console.log('ImportMeta env: ', import.meta.env);
-console.log('ImportMeta VITE_API_URL: ', import.meta.env.VITE_API_URL);
-
 // Базовые настройки
 const httpConfig: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
